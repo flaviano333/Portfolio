ef8902fdd4f837c39817c8a40eff1455e74a31fc
faviano333 <flaviano333.fg@gmail.com>
2024-08-31 20:12:11 -0300

    7

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -52,8 +52,8 @@ const Carousel = () => {
       </div>
       <div className="carousel-container">
         <Slider {...settings}>
-        <div>
-          <iframe src="https://www.youtube.com/embed/_R6MHJ-Gtxk?si=UVHhYh8hV3KjKNgo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+          <div>
+          <iframe width={100%} src="https://www.youtube.com/embed/_R6MHJ-Gtxk?si=UVHhYh8hV3KjKNgo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
           </div>          
           <div>
             <ImageComponent imagePath="gs://portfolio-75cc2.appspot.com/aladdin mega drive.jpg" alt="Slide 1" />
