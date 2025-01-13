6eb160e4e1beb3d248a2bb6752e5dcc6f41eab2a
Flávio de Oliveira <85568107+flaviano333@users.noreply.github.com>
2025-01-12 22:04:54 -0300

    Update bio.jsx

--- src/components/bio.jsx
+++ src/components/bio.jsx
@@ -27,7 +27,7 @@ const Bio = () => {
           <Typography variant="body1" style={{ color: '#e0e0e0', fontSize: '1.5rem', fontWeight: '400', marginBottom: '10px', marginLeft: '10%', fontFamily:'Ubuntu' }}>
           Olá, meu nome é Flávio de Oliveira, tenho 19 anos e sou estudante de Design na <b>Universidade de Brasília (UnB)</b>. Em 2023, concluí o curso de Técnico em Informática na <b>Escola Técnica de Brasília (ETB)</b>, o que me proporcionou uma base sólida em desenvolvimento web. Desde então, tenho me dedicado ao aprimoramento dos meus projetos pessoais, especialmente no contexto de design.
 <p></p>
-Atualmente, estou em busca de oportunidades no mercado de trabalho como designer. Apesar de ainda não ter experiência profissional formal, organizei meus projetos pessoais em um portfólio, que inclui principalmente trabalhos relacionados a videogames e streaming. Estou ansioso para aplicar minhas habilidades e conhecimentos em novos desafios profissionais.
+Atualmente, sou estagiário de Design no Itamaraty (Ministério das Relações Exteriores). Aqui eu organizei meus projetos pessoais em um portfólio, que inclui principalmente trabalhos relacionados a videogames e streaming. Estou ansioso para aplicar minhas habilidades e conhecimentos em novos desafios profissionais.
           </Typography>
         </Grid>
       </Grid>     
